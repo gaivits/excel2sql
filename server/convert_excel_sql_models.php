736b32251ab1798ff4c6088e1e5334959774527c
gaivits <49124734+gaivits@users.noreply.github.com>
2024-02-15 09:38:38 +0700

    Update convert_excel_sql_models.php

--- server/convert_excel_sql_models.php
+++ server/convert_excel_sql_models.php
@@ -1,23 +1,24 @@
 <?php
 require_once("../vendor/autoload.php");
 require_once("../connections.php");
-
-
 use PhpOffice\PhpSpreadsheet\IOFactory;
+
 
 class convert_excel_sql_models extends connections
 {
 
     function insert_from_excel($files)
     {
+        
         $conn = $this->oracleMyConn();
-
+        
         // Specify the path to your Excel file
-        $excelFilePath = "../documents/{$files}";
 
-        // Load the Excel file
+        $excelFilePath = '/usr/share/nginx/erp.365supplychain.com/upload_price_table/documents/'.$files;
+        
+                // Load the Excel file
         $spreadsheet = IOFactory::load($excelFilePath);
-
+        
         // Get the first sheet in the Excel file
         $sheet = $spreadsheet->getActiveSheet();
 
@@ -42,56 +43,54 @@ class convert_excel_sql_models extends connections
                     $new_name_col = 'PC_PART';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'G') {
+                if ($col == 'C') {
                     $new_name_col = 'PC_UM';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'H') {
+                if ($col == 'D') {
                     $new_name_col = 'PC_MIN_PRICE';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'I') {
+                if ($col == 'E') {
                     $new_name_col = 'PC_MAX_PRICE##1';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'J') {
+                if ($col == 'F') {
                     $new_name_col = 'PC_MAX_PRICE##2';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'K') {
+                if ($col == 'G') {
                     $new_name_col = 'PC_MAX_PRICE##3';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'L') {
+                if ($col == 'H') {
                     $new_name_col = 'PC_MAX_PRICE##4';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'M') {
+                if ($col == 'I') {
                     $new_name_col = 'PC_MAX_PRICE##5';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'N') {
+                if ($col == 'J') {
                     $new_name_col = 'PC_MAX_PRICE##6';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'O') {
+                if ($col == 'K') {
                     $new_name_col = 'PC_START';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
-                if ($col == 'P') {
+                if ($col == 'L') {
                     $new_name_col = 'PC_EXPIRE';
                     $prepare_data[$row][$new_name_col] = $cellValue;
                 }
             }
         }
-    
+	
         for ($i = 2; $i < count($prepare_data); $i++) {
-            
+
             $PC_LIST = $prepare_data[$i]['PC_LIST'];
             $PC_PART = $prepare_data[$i]['PC_PART'];
             $PC_UM = $prepare_data[$i]['PC_UM'];
-            $PC_START = strtotime($prepare_data[$i]['PC_START']) || '09-06-2024';
-            $PC_EXPIRE = strtotime($prepare_data[$i]['PC_EXPIRE']) || 'now+25 years';
             $PC_MIN_PRICE = $prepare_data[$i]['PC_MIN_PRICE'];
             $PC_MAX_PRICE_1 = $prepare_data[$i]['PC_MAX_PRICE##1'];
             $PC_MAX_PRICE_2 = $prepare_data[$i]['PC_MAX_PRICE##2'];
@@ -99,92 +98,120 @@ class convert_excel_sql_models extends connections
             $PC_MAX_PRICE_4 = $prepare_data[$i]['PC_MAX_PRICE##4'];
             $PC_MAX_PRICE_5 = $prepare_data[$i]['PC_MAX_PRICE##5'];
             $PC_MAX_PRICE_6 = $prepare_data[$i]['PC_MAX_PRICE##6'];
-            $sql = 'INSERT INTO TSTTEST.PC_MSTR ("U##PC_LIST",
-         PC_LIST, 
-         "U##PC_PROD_LINE", 
-         PC_PROD_LINE, 
-         "U##PC_PART", 
-         PC_PART, 
-         "U##PC_UM", 
-         PC_UM, 
-         PC_START,
-         PC_EXPIRE, 
-         PC_AMT_TYPE, 
-         PC__QAD02, 
-         "PC_MIN_QTY##1", 
-         "PC_MIN_QTY##2", 
-         "PC_MIN_QTY##3", 
-         "PC_MIN_QTY##4", 
-         "PC_MIN_QTY##5", 
-         "PC_MIN_QTY##6", 
-         "PC_MIN_QTY##7", 
-         "PC_MIN_QTY##8", 
-         "PC_MIN_QTY##9", 
-         "PC_MIN_QTY##10", 
-         "PC_MIN_QTY##11", 
-         "PC_MIN_QTY##12", 
-         "PC_MIN_QTY##13", 
-         "PC_MIN_QTY##14", 
-         "PC_MIN_QTY##15", 
-         "PC_AMT##1", 
-         "PC_AMT##2", 
-         "PC_AMT##3", 
-         "PC_AMT##4", 
-         "PC_AMT##5", 
-         "PC_AMT##6", 
-         "PC_AMT##7", 
-         "PC_AMT##8", 
-         "PC_AMT##9", 
-         "PC_AMT##10", 
-         "PC_AMT##11", 
-         "PC_AMT##12", 
-         "PC_AMT##13", 
-         "PC_AMT##14", 
-         "PC_AMT##15", 
-         "U##PC_CURR", 
-         PC_CURR, 
-         PC_TAX_IN, 
-         PC__QAD01, 
-         PC_USER1, 
-         PC_USER2, 
-         PC_MOD_DATE, 
-         PC_USERID, 
-         PC_MIN_PRICE, 
-         "PC_MAX_PRICE##1", 
-         "PC_MAX_PRICE##2", 
-         "PC_MAX_PRICE##3", 
-         "PC_MAX_PRICE##4", 
-         "PC_MAX_PRICE##5", 
-         "PC_MAX_PRICE##6", 
-         "PC_MAX_PRICE##7", 
-         "PC_MAX_PRICE##8", 
-         "PC_MAX_PRICE##9", 
-         "PC_MAX_PRICE##10", 
-         PROGRESS_RECID, 
-         PC_COMM_SALE, 
-         PC_COMM_MGR) VALUES(NULL,:PC_LIST, NULL, NULL, NULL, :PC_PART, NULL, :PC_UM, :PC_START, :PC_EXPIRE, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, :PC_MIN_PRICE, :PC_MAX_PRICE_1, :PC_MAX_PRICE_2, :PC_MAX_PRICE_3, :PC_MAX_PRICE_4, :PC_MAX_PRICE_5, :PC_MAX_PRICE_6, NULL, NULL, NULL, NULL, NULL, NULL, NULL)';
-
-
-
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':PC_LIST', $PC_LIST,PDO::PARAM_STR);
-            $stmt->bindParam(':PC_PART', $PC_PART,PDO::PARAM_STR);
-            $stmt->bindParam(':PC_UM', $PC_UM,PDO::PARAM_STR);
-            $stmt->bindParam(':PC_START', $PC_START || strtotime('06-09-2023'),PDO::PARAM_STR);
-            $stmt->bindParam(':PC_EXPIRE', $PC_EXPIRE || strtotime('now+25 years'),PDO::PARAM_STR);
-            $stmt->bindParam(':PC_MIN_PRICE', $PC_MIN_PRICE,PDO::PARAM_STR);
-            $stmt->bindParam(':PC_MAX_PRICE_1', $PC_MAX_PRICE_1,PDO::PARAM_STR);
-            $stmt->bindParam(':PC_MAX_PRICE_2', $PC_MAX_PRICE_2,PDO::PARAM_STR);
-            $stmt->bindParam(':PC_MAX_PRICE_3', $PC_MAX_PRICE_3,PDO::PARAM_STR);
-            $stmt->bindParam(':PC_MAX_PRICE_4', $PC_MAX_PRICE_4,PDO::PARAM_STR);
-            $stmt->bindParam(':PC_MAX_PRICE_5', $PC_MAX_PRICE_5,PDO::PARAM_STR);
-            $stmt->bindParam(':PC_MAX_PRICE_6', $PC_MAX_PRICE_6,PDO::PARAM_STR);
-            try
+            $PC_START = explode( "/",$prepare_data[$i]['PC_START'] );
+            $PAT_PC_START = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/\d{2}$';
+            if(preg_match($PAT_PC_START,$PC_START))
             {
-                $stmt->execute();
+                $NEW_PC_START = $PC_START;
+                if($NEW_PC_START[1]=='01')
+                {
+                    $NEW_PC_START[1] = 'JAN';
+                }
+                if($NEW_PC_START[1]=='02')
+                {
+                    $NEW_PC_START[1] = 'FEB';
+                }
+                if($NEW_PC_START[1]=='03')
+                {
+                    $NEW_PC_START[1] = 'MAR';
+                }
+                if($NEW_PC_START[1]=='04')
+                {
+                    $NEW_PC_START[1] = 'APR';
+                }
+                if($NEW_PC_START[1]=='05')
+                {
+                    $NEW_PC_START[1] = 'MAY';
+                }
+                if($NEW_PC_START[1]=='06')
+                {
+                    $NEW_PC_START[1] = 'JUNE';
+                }
+                if($NEW_PC_START[1]=='07')
+                {
+                    $NEW_PC_START[1] = 'JUL';
+                }
+                if($NEW_PC_START[1]=='08')
+                {
+                    $NEW_PC_START[1] = 'AUG';
+                }
+                if($NEW_PC_START[1]=='09')
+                {
+                    $NEW_PC_START[1] = 'SEP';
+                }
+                if($NEW_PC_START[1]=='10')
+                {
+                    $NEW_PC_START[1] = 'OCT';
+                }
+                if($NEW_PC_START[1]=='11')
+                {
+                    $NEW_PC_START[1] = 'NOV';
+                }
+                if($NEW_PC_START[1]=='12')
+                {
+                    $NEW_PC_START[1] = 'DEC';
+                }
+                $PC_START = $NEW_PC_START[0].'-'.$NEW_PC_START[1].'-'.$NEW_PC_START[2];
             }
-            catch (PDOException $e)
-            {
+            
+
+
+            // $PC_MIN_PRICE = $prepare_data[$i]['PC_MIN_PRICE'];
+            // $PC_MAX_PRICE_1 = $prepare_data[$i]['PC_MAX_PRICE##1'];
+            // $PC_MAX_PRICE_2 = $prepare_data[$i]['PC_MAX_PRICE##2'];
+            // $PC_MAX_PRICE_3 = $prepare_data[$i]['PC_MAX_PRICE##3'];
+            // $PC_MAX_PRICE_4 = $prepare_data[$i]['PC_MAX_PRICE##4'];
+            // $PC_MAX_PRICE_5 = $prepare_data[$i]['PC_MAX_PRICE##5'];
+            // $PC_MAX_PRICE_6 = $prepare_data[$i]['PC_MAX_PRICE##6'];
+            $sql = 'INSERT INTO TSTTEST.pc_mstr(
+                PC_LIST,
+                PC_PART,
+                PC_UM,
+                PC_MIN_PRICE,
+                "PC_MAX_PRICE##1",
+                "PC_MAX_PRICE##2",
+                "PC_MAX_PRICE##3",
+                "PC_MAX_PRICE##4",
+                "PC_MAX_PRICE##5",
+                "PC_MAX_PRICE##6",
+                PC_START
+               
+                
+                
+         
+         ) VALUES(:PC_LIST,
+         :PC_PART,
+         :PC_UM,
+         :PC_MIN_PRICE,
+         :PC_MAX_PRICE_1,
+         :PC_MAX_PRICE_2,
+         :PC_MAX_PRICE_3,
+         :PC_MAX_PRICE_4,
+         :PC_MAX_PRICE_5,
+         :PC_MAX_PRICE_6,
+         PC_START
+         )';
+
+
+            
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':PC_LIST', $PC_LIST, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_PART', $PC_PART, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_UM', $PC_UM, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_MIN_PRICE', $PC_MIN_PRICE, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_MAX_PRICE_1', $PC_MAX_PRICE_1, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_MAX_PRICE_2', $PC_MAX_PRICE_2, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_MAX_PRICE_3', $PC_MAX_PRICE_3, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_MAX_PRICE_4', $PC_MAX_PRICE_4, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_MAX_PRICE_5', $PC_MAX_PRICE_5, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_MAX_PRICE_6', $PC_MAX_PRICE_6, PDO::PARAM_STR);
+            $stmt->bindParam(':PC_START', $PC_START, PDO::PARAM_STR);
+
+
+
+            try {
+                $stmt->execute();
+            } catch (PDOException $e) {
                 echo $e;
             }
         }
